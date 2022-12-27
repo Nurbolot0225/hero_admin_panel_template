a72eb6420d3c20765dbe6d8d09765b1d61f684ca
Nurbolot Berdibekov <nurchik0225@gmail.com>
2022-12-27 23:23:28 +0600

    Redux-Thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import ReduxThunk from 'redux-thunk'
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -28,7 +29,7 @@ const enhancer = (createStore) => (...args) => {
 
 const store = createStore( 
                 combineReducers({heroes, filters}),
-                compose(applyMiddleware(stringMiddleware),
+                compose(applyMiddleware(ReduxThunk, stringMiddleware),
                     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
                 );
 
