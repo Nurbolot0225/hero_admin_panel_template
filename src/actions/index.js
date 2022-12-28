0e9e0dfa358fd4b0776763055f7c7633a6016e56
Nurbolot Berdibekov <nurchik0225@gmail.com>
2022-12-28 15:25:58 +0600

    createAsyncThunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,4 @@
-import {heroesFetching, heroesFetched, heroesFetchingError} from '../components/heroesList/heroesSlice';
 import {filtersFetching, filtersFetched, filtersFetchingError} from '../components/heroesFilters/filtersSlice';
-export const fetchHeroes = (request) => (dispatch) => {
-    dispatch(heroesFetching());
-    request("http://localhost:3001/heroes")
-        .then(data => dispatch(heroesFetched(data)))
-        .catch(() => dispatch(heroesFetchingError()))
-}
 
 export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching());
